7e4f3fb72f97d8b0e77c9c326e1ab060865ad616
TheAssassin <theassassin@assassinate-you.net>
2020-06-29 00:44:27 +0200

    Improve CLI

--- libreverse/__main__.py
+++ libreverse/__main__.py
@@ -1,7 +1,39 @@
+import os
+import sys
+from argparse import ArgumentParser
+
 from flask_frozen import Freezer
 from .app_factory import create_app
 
-app = create_app()
-freezer = Freezer(app)
 
-freezer.freeze()
+def main():
+    parser = ArgumentParser("libreverse")
+    subparsers = parser.add_subparsers()
+
+    freeze_parser = subparsers.add_parser("freeze")
+    freeze_parser.add_argument("models_dir")
+    freeze_parser.add_argument("out_dir", nargs="?")
+
+    args = parser.parse_args()
+
+    app_config = {}
+
+    if "models_dir" in args:
+        app_config["MODELS_DIR"] = args.models_dir
+
+        if "out_dir" in args and args.out_dir:
+            app_config["FREEZER_DESTINATION"] = os.path.abspath(args.out_dir)
+
+        app = create_app(app_config)
+
+        freezer = Freezer(app)
+
+        freezer.freeze()
+        return 0
+
+    else:
+        parser.print_usage()
+        return 1
+
+
+sys.exit(main())
